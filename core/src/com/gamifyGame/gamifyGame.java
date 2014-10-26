a90430ade672767c27cc1f954357f1b4dafb6b94
stephenp <Sythe2o0@gmail.com>
2014-10-26 12:33:16 -0500

    Added demo GUI Screen, with game background and five boxes to represent the eventual five things we're going to put in those boxes. Wrote drawCenter(Texture, int hOffset, int vOffset) to make drawing things close to the center less wordy. Considered writing a general draw wrapper which draws at-(imgWidth /2), -(imgHeight /2) to place images at their center instead of at their bottom left corner.

--- core/src/com/gamifyGame/gamifyGame.java
+++ core/src/com/gamifyGame/gamifyGame.java
@@ -9,19 +9,33 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class gamifyGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+    Texture background;
+    static int scrWidth = 720;
+	static int scrHeight = 1200;
 
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+        background = new Texture("Gamify Mockup B.png");
 	}
+
+    public void drawCenter(Texture img, int hOffset, int vOffset){
+        int iWidth = img.getWidth();
+        int iHeight = img.getHeight();
+        batch.draw(img, (scrWidth/2)-(iWidth/2)+hOffset, (scrHeight/2)-(iHeight/2)+vOffset);
+    }
 
 
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+        batch.draw(background,0,0, scrWidth, scrHeight);
+        drawCenter(img,150,200);
+        drawCenter(img,-150,200);
+        drawCenter(img,150,-100);
+        drawCenter(img,-150,-100);
+		drawCenter(img,0,50);
 		batch.end();
 	}
 }
