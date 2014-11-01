6f2f2cd34cbddec37f15898a3cb01eec7347ddef
Stephen <sythe2o0@gmail.com>
2014-11-01 12:54:46 -0500

    In gameScreen, there is now many new things: A Stage, a Viewport, and a bunch of Images instead of Textures. This will need to be done to the other screen as well, however it is worth noting we could instead make each screen a new stage with each stage drawn only as necessary. This would reduce image and function duplication between screens.

--- core/src/com/gamifyGame/gameScreen.java
+++ core/src/com/gamifyGame/gameScreen.java
@@ -1,79 +1,105 @@
 package com.gamifyGame;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Game;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import java.lang.Math;
 
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.utils.Scaling;
+import com.badlogic.gdx.utils.viewport.ScalingViewport;
 
 
 public class gameScreen implements Screen{
     SpriteBatch batch;
-    Texture midbox;
-    Texture quad1;
-    Texture quad2;
-    Texture quad3;
-    Texture quad4;
-    Texture background;
-    Texture itemBar;
+    Image background;
+    Image itemBar;
     static int scrWidth;
     static int scrHeight;
     BitmapFont font;
 
     gamifyGame game;
 
+    Stage layer1;
+
+    float Ax,A2x,A5x,Ay,A2y,A5y,Az,A2z,A5z;
+
+    int framecount;
+
     public gameScreen(gamifyGame game){
         this.game = game;
         scrWidth = Gdx.graphics.getWidth();
         scrHeight = Gdx.graphics.getHeight();
+
+        ScalingViewport view = new ScalingViewport(Scaling.stretch, 180, 296);
+
+        layer1 = new Stage(view);
+
         batch = new SpriteBatch();
-        background = new Texture("Background180x296.png");
-        itemBar = new Texture("ItemBar.png");
+        background = imageSetup("Background180x296.png", layer1, 0, 0);
+        itemBar = imageSetup("ItemBar.png",layer1,0,254);
         font = new BitmapFont();
+        framecount = 0;
+
+        Ax = Gdx.input.getAccelerometerX();
+        Ay = Gdx.input.getAccelerometerY();
+        Az = Gdx.input.getAccelerometerZ();
     }
 
+    /*
+     **********Image Setup functions***********
+                                              */
 
-    public void drawCenter(Texture img, int hOffset, int vOffset){
-        // Default WMult and HMult are 4 because we are rendering everything
-        // at x4 their default dimensions.
-        drawCenter(img,hOffset,vOffset,4,4);
+    public Image imageSetupCenter(String file, Stage stage, int hOffset, int vOffset){
+        return imageSetup(file,stage,(scrWidth/2)+hOffset,(scrHeight)/2+vOffset);
     }
 
-    public void drawCenter(Texture img, int hOffset, int vOffset, int wMult, int hMult){
-        int iWidth = img.getWidth();
-        int iHeight = img.getHeight();
-        batch.draw(img, (scrWidth / 2) - (iWidth * wMult / 2) + hOffset,
-                (scrHeight / 2) - (iHeight * hMult / 2) + vOffset, iWidth * wMult, iHeight * hMult);
+    public Image imageSetup(String file, Stage stage, int hOrigin, int vOrigin){
+        Texture texture = new Texture(file);
+        Image image = new Image(texture);
+        image.setPosition(hOrigin,vOrigin);
+        image.setSize(texture.getWidth(),texture.getHeight());
+        stage.addActor(image);
+        return image;
     }
-
 
     public void render (float delta) {
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        batch.begin();
-        batch.draw(background,0,0, scrWidth, scrHeight);
-        batch.draw(itemBar,0,scrHeight-(itemBar.getHeight()*4), scrWidth,itemBar.getHeight()*4);
+
+        layer1.draw();
         font.setColor(1.0f, 1.0f, 1.0f, 1.0f);
 
+        batch.begin();
         // ***** DEBUG PRINTING ***** //
-
-        float Ax = Gdx.input.getAccelerometerX();
-        //float DAx = Ax * Gdx.input.getDeltaX();
-        float Ay = Gdx.input.getAccelerometerY();
-        //float DAy = Ay * Gdx.input.getDeltaY();
-        float Az = Gdx.input.getAccelerometerZ();
-        //font.draw(batch, String.valueOf(DAx), 50, 100);
-        //font.draw(batch, String.valueOf(DAy), 50, 80);
-        font.draw(batch, String.valueOf(Ax),50,60);
-        font.draw(batch, String.valueOf(Ay),50,40);
-        font.draw(batch, String.valueOf(Az),50,20);
+        if (framecount % 5 == 0) {
+            A5x = A2x;
+            A5y = A2y;
+            A5z = A2z;
+            A2x = Ax;
+            A2y = Ay;
+            A2z = Az;
+            Ax = Gdx.input.getAccelerometerX();
+            Ay = Gdx.input.getAccelerometerY();
+            Az = Gdx.input.getAccelerometerZ();
+        }
+        font.draw(batch, String.valueOf(Math.abs(A5x - Ax)), 50, 180);
+        font.draw(batch, String.valueOf(Math.abs(A5y - Ay)), 50, 160);
+        font.draw(batch, String.valueOf(Math.abs(A5z - Az)), 50, 140);
+        font.draw(batch, String.valueOf(Ax), 50, 60);
+        font.draw(batch, String.valueOf(Ay), 50, 40);
+        font.draw(batch, String.valueOf(Az), 50, 20);
 
         batch.end();
-
+        if (framecount == 30){
+            framecount = 0;
+        }
+        framecount += 1;
         if (Gdx.input.justTouched()) // http://libgdx.badlogicgames.com/nightlies/docs/api/com/badlogic/gdx/Input.html
             game.setScreen(game.mainS);
     }
